36c6207d4e6b652735d0488cc4da5813290c993a
Mayank Sethi <mayank@digibrain.co.in>
2024-07-11 15:36:20 +0530

    collect user input and add projectData to projectState

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,25 @@ const App = () => {
     }));
   };
 
+  const handleAddProject = (projectData) => {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
+  console.log(projectState);
+
   let content;
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProjects} />;
   }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,5 +1,22 @@
+import { useRef } from 'react';
+
 import Input from './Input';
-const NewProject = () => {
+const NewProject = ({ onAdd }) => {
+  const titleRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const dueDateRef = useRef(null);
+
+  const handleSave = () => {
+    const title = titleRef.current.value;
+    const description = descriptionRef.current.value;
+    const dueDate = dueDateRef.current.value;
+    const project = { title, description, dueDate };
+
+    // validation ...
+
+    onAdd(project);
+  };
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -9,15 +26,18 @@ const NewProject = () => {
           </button>
         </li>
         <li>
-          <button className="rounded-md px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="rounded-md px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" type="text" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" type="date" />
+        <Input label="Title" type="text" ref={titleRef} />
+        <Input label="Description" textarea ref={descriptionRef} />
+        <Input label="Due Date" type="date" ref={dueDateRef} />
       </div>
     </div>
   );
